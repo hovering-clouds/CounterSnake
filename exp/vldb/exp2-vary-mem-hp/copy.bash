172e613f13a6a154e53809ddadfd21d0a652cbc7
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2025-06-02 15:53:29 +0000

    config for exp2-hp

---
+++ exp/vldb/exp2-vary-mem-hp/copy.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+old_pfx="80M"
+new_pfx="100M"
+
+mkdir -p config_${new_pfx}
+for f in config_${old_pfx}/*; do
+  if [[ -f "$f" ]]; then
+    filename="${f##*/}"  # 提取文件名（去掉路径）
+    if [[ "${filename%.*}" == *${old_pfx} ]]; then
+      # 构建新文件名（替换1M为2M并保留扩展名）
+      newname="${filename%.*}"
+      newname="${newname%"$old_pfx"}${new_pfx}"
+      [[ "$filename" == *.* ]] && newname+=".${filename##*.}"
+      # 复制到目标目录
+      cp -- "$f" "config_${new_pfx}/$newname"
+    fi
+  fi
+done
