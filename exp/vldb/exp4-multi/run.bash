57b40326050de4ad391cfaeca67ef73846c37dae
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2025-06-26 09:23:47 +0000

    config files for exp4

---
+++ exp/vldb/exp4-multi/run.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd '/home/liu_xunpeng/CounterSnake/build'
+
+# 遍历配置
+suffixes=("all" "cm" "dt" "es" "fr" "hp" "mv" "pr" "sl")
+for suffix in "${suffixes[@]}"; do
+  config_file="../exp/vldb/exp4-multi/exp4-${suffix}.toml"
+
+  # 检查配置文件是否存在
+  if [[ -f "$config_file" ]]; then
+    echo "Running: ./Dway -c $config_file"
+    "./Dway" -c "$config_file"
+  else
+    echo "Warning: Config file '$config_file' not found, skipping."
+  fi
+done
