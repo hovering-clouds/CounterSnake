fb0a4150a1815335d72c617df813c13acdfa7022
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2025-06-26 09:24:19 +0000

    config files and executable for exp6

---
+++ exp/vldb/exp6-layer/run.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd '/home/liu_xunpeng/CounterSnake/exp/vldb/exp6-layer'
+
+# 遍历配置
+suffixes=("1" "2" "3" "4" "5" "6")
+for suffix in "${suffixes[@]}"; do
+  exec_path="./Dway_${suffix}"
+  config_file="./dway_${suffix}.toml"
+
+  # 检查配置文件是否存在
+  if [[ -f "$config_file" ]]; then
+    echo "Running: $exec_path -c $config_file"
+    "$exec_path" -c "$config_file"
+  else
+    echo "Warning: Config file '$config_file' not found, skipping."
+  fi
+done
